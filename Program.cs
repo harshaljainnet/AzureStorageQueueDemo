922ebe9524c3331ed8bed5d249761dbcab86ce6f
Harshalkumar Jain <harshal.jain.net@gmail.com>
2025-03-27 09:45:08 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Azure.Storage.Queues;
+
+
+
+var queueName = "";
+var storageConnectionString = "";
+
+var queueClient = new QueueClient(storageConnectionString, queueName);
+
+// Send message to storage queue
+//await queueClient.SendMessageAsync("Hello From C# Visual Studio - msg2");
+//Console.WriteLine("Message has been sent....");
+
+ // receive message from storage queue
+var messages = await queueClient.ReceiveMessagesAsync();
+Console.WriteLine("Message has been received....");
+
+// business logic and work with messages
+
+// delete message in storage queue
+await queueClient.DeleteMessageAsync(messages.Value[0].MessageId, messages.Value[0].PopReceipt);
+Console.WriteLine("Message has been deleted....");
+
